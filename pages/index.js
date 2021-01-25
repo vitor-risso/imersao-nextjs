9060747ff515c2dfefb8124a3fdde90ebf3e5175
vitor-risso <vitor.risso13@gmail.com>
2021-01-25 11:53:47 -0300

    finalizando a aula 01, base do projeto criada

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,45 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
-}
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda </h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum dolor sit amet</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+              <h1>The legend of zelda </h1>
+              <p>Lorem ipsum dolor sit amet</p>
+            </Widget.Content>
+        </Widget>
+        <Footer />
+        <GitHubCorner projectUrl='https://github.com/vitor-risso/imersao-nextjs' />
+      </QuizContainer>
+    </QuizBackground>
+  )
+};
